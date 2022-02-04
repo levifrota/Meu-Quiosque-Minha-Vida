199772f9f69e9e062ae0bfd744fbe3f3f6af18b4
Gideão Frota <gideao.frota@enext.com.br>
2022-02-03 21:39:57 -0300

    header atualizado(mas não finalizado)

--- frontend/header.js
+++ frontend/header.js
@@ -1,27 +1,7 @@
-let searchForm = document.querySelector(".search-form");
-
-document.querySelector("#search-btn").onclick = () => {
-  searchForm.classList.toggle("active");
-  shoppingCart.classList.remove("active");
-  loginForm.classList.remove("active");
-  navbar.classList.remove("active");
-};
-
-let shoppingCart = document.querySelector(".shopping-cart");
-
-document.querySelector("#cart-btn").onclick = () => {
-  shoppingCart.classList.toggle("active");
-  searchForm.classList.remove("active");
-  loginForm.classList.remove("active");
-  navbar.classList.remove("active");
-};
-
 let loginForm = document.querySelector(".login-form");
 
 document.querySelector("#login-btn").onclick = () => {
   loginForm.classList.toggle("active");
-  searchForm.classList.remove("active");
-  shoppingCart.classList.remove("active");
   navbar.classList.remove("active");
 };
 
@@ -29,14 +9,10 @@ let navbar = document.querySelector(".navbar");
 
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
-  searchForm.classList.remove("active");
-  shoppingCart.classList.remove("active");
   loginForm.classList.remove("active");
 };
 
 window.onscroll = () => {
-  searchForm.classList.remove("active");
-  shoppingCart.classList.remove("active");
   loginForm.classList.remove("active");
   navbar.classList.remove("active");
 };
